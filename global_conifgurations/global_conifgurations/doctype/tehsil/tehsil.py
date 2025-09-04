9028c856d6be8fc80c07705e745326acbcf6316c
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-09-04 10:16:37 +0000

    district, tehsil, uc added.

---
+++ global_conifgurations/global_conifgurations/doctype/tehsil/tehsil.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, nabeel saleem and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Tehsil(Document):
+	pass
